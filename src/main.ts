670f922dfa11137a9a842f58a31c82bd33156f12
Jerry Tan <jerrytan7@gmail.com>
2025-01-21 10:45:32 -0500

    add cors middleware

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
+  app.enableCors({
+    origin: '*', // Allow requests from Vite frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
