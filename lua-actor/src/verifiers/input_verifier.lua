a9dc9cd79936788cedd7ae2008b171ad09411c4f
Victor Leipnik <vic@sobol.io>
2025-04-04 09:28:50 -0400

    Splitting out logic for better testability

---
+++ lua-actor/src/verifiers/input_verifier.lua
@@ -0,0 +1,49 @@
+local InputVerifier = {}
+
+-- Default verifiers
+local defaultVerifiers = {
+    VerifiedCredentialEIP712 = function(input, value)
+        -- TODO: Implement actual EIP712 verification
+        return true
+    end,
+    
+    EVMTransaction = function(input, value)
+        -- TODO: Implement actual EVM transaction verification
+        return true
+    end
+}
+
+function InputVerifier.new(customVerifiers)
+    local self = {
+        verifiers = {}
+    }
+
+    -- Merge default verifiers with custom ones
+    for k, v in pairs(defaultVerifiers) do
+        self.verifiers[k] = v
+    end
+    if customVerifiers then
+        for k, v in pairs(customVerifiers) do
+            self.verifiers[k] = v
+        end
+    end
+
+    setmetatable(self, { __index = InputVerifier })
+    return self
+end
+
+function InputVerifier:verify(inputType, input, value)
+    local verifier = self.verifiers[inputType]
+    if not verifier then
+        return false, string.format("Unsupported input type: %s", inputType)
+    end
+
+    local isValid, errorMsg = verifier(input, value)
+    if not isValid then
+        return false, string.format("Input validation failed: %s", errorMsg)
+    end
+
+    return true, nil
+end
+
+return InputVerifier 
