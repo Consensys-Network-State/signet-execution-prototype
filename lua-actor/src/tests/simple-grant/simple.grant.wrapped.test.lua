2a5b42034f1d0c909b017981107e3805b7a2ad9b
Victor Leipnik <vic@sobol.io>
2025-05-20 14:55:42 -0400

    Refactoring where tests are located and how they are named

--- lua-actor/src/tests/simple-grant/simple.grant.wrapped.test.lua
+++ lua-actor/src/tests/simple-grant/simple.grant.wrapped.test.lua
@@ -8,11 +8,11 @@ local DFSM = require("dfsm")
 local TestUtils = require("test-utils")
 
 -- Load agreement document from JSON file
-local agreementDoc = TestUtils.loadInputDoc("./test-data/simple-grant/simple.grant.wrapped.json")
-local inputA = TestUtils.loadInputDoc("./test-data/simple-grant/simple.grant.partyA-input.wrapped.json")
-local inputB = TestUtils.loadInputDoc("./test-data/simple-grant/simple.grant.partyB-input.wrapped.json")
-local inputAAccept = TestUtils.loadInputDoc("./test-data/simple-grant/simple.grant.partyA-input-accept.wrapped.json")
-local inputAReject = TestUtils.loadInputDoc("./test-data/simple-grant/simple.grant.partyA-input-reject.wrapped.json")
+local agreementDoc = TestUtils.loadInputDoc("./tests/simple-grant/simple.grant.wrapped.json")
+local inputA = TestUtils.loadInputDoc("./tests/simple-grant/simple.grant.partyA-input.wrapped.json")
+local inputB = TestUtils.loadInputDoc("./tests/simple-grant/simple.grant.partyB-input.wrapped.json")
+local inputAAccept = TestUtils.loadInputDoc("./tests/simple-grant/simple.grant.partyA-input-accept.wrapped.json")
+local inputAReject = TestUtils.loadInputDoc("./tests/simple-grant/simple.grant.partyA-input-reject.wrapped.json")
 
 local dfsm = DFSM.new(agreementDoc, true)
 
